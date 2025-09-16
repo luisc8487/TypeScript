cd819ee91ad73441ae7f652095dcb0749e6ff2dc
L C <LF9904@gmail.com>
2025-09-15 20:17:57 -0400

    added Function type annotations

---
+++ FunctionTypeAnnotations.ts
@@ -0,0 +1,6 @@
+// Function with type annotations for parameters and return type
+// This function takes two numbers and returns their sum
+
+function add(x: number, y: number): number {
+  return x + y;
+}
