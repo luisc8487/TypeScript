ec206f449879478ad702b89aff0fd40769cbab74
L C <LF9904@gmail.com>
2025-09-16 00:30:58 -0400

    commented entire file b/c react is not imported

--- FunctionTypePropsInterface.ts
+++ FunctionTypePropsInterface.ts
@@ -1,25 +1,25 @@
-// Function Type Props Interface in TypeScript
-// This interface defines a function type that takes two parameters and returns a string.
+// // Function Type Props Interface in TypeScript
+// // This interface defines a function type that takes two parameters and returns a string.
 
-// import React from "react";
+// // import React from "react";
 
-// Defining the interface for the component props
-interface ColorPickerProps {
-  // An array of color strings
-  color: string[];
-  // A function that takes a color string and returns void
-  handleColorSelect: (color: string) => void;
-}
+// // Defining the interface for the component props
+// interface ColorPickerProps {
+//   // An array of color strings
+//   color: string[];
+//   // A function that takes a color string and returns void
+//   handleColorSelect: (color: string) => void;
+// }
 
-// A React functional component that uses the ColorPickerProps interface for its props
-function ColorPicker({color, handleColorSelect}: ColorPickerProps) {
-  const renderColor = color.map((c) => {
-    return (
-      <button key={c} onClick={() => handleColorSelect(c)}>
-        {c}
-      </button>
-    );
-  });
+// // A React functional component that uses the ColorPickerProps interface for its props
+// function ColorPicker({color, handleColorSelect}: ColorPickerProps) {
+//   const renderColor = color.map((c) => {
+//     return (
+//       <button key={c} onClick={() => handleColorSelect(c)}>
+//         {c}
+//       </button>
+//     );
+//   });
 
-  return <div>{renderColor}</div>;
-}
+//   return <div>{renderColor}</div>;
+// }
